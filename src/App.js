2334dc5ba1ddd79dd1307f93a853c56beaf52ef7
isehyun628 <isehyun628@gmail.com>
2026-07-15 21:05:36 +0900

    feat: 로그인 페이지, 그룹 목록 페이지 이동 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 페이지 이동에 필요한 기능
+
+import LoginPage from "./pages/LoginPage";
 import GroupPage from "./pages/GroupPage";
 import CreateGroupPage from "./pages/CreateGroupPage";
 // 페이지 불러오기
@@ -8,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<GroupPage />} />
+        <Route path="/" element={<LoginPage />} />
+
+        <Route path="/groups" element={<GroupPage />} />
 
         <Route
           path="/groups/create"
